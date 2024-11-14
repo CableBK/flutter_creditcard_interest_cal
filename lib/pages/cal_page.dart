e2e10c544d01290f7643759aa016e8f97bfd12ac
เคเบิล <cbkcontactmail@gmail.com>
2024-11-14 23:32:36 +0700

    change gradle to 8.8

--- lib/pages/cal_page.dart
+++ lib/pages/cal_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 
 class CalPage extends StatefulWidget {
@@ -8,6 +10,14 @@ class CalPage extends StatefulWidget {
 }
 
 class _CalPageState extends State<CalPage> {
+  var cash = 3000;
+  var day = 9;
+  var interest = 16;
+  var year = 365;
+  var total1 = 0.0;
+  var cash2 = 7000;
+  var total3 = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +27,16 @@ class _CalPageState extends State<CalPage> {
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
-          children: [Row()],
+          children: [
+            Row(
+              children: [
+                Text('จำนวนที่จ่าย $cash'),
+                Text('ดอกเบี้ย $interest %'),
+                Text(
+                    'สรุป ${(total1 = (cash * (interest / 100) * day) / year)}'),
+              ],
+            )
+          ],
         ),
       ),
     );
