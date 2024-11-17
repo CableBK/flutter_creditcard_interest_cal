1fc3b10dc00907e1968b78d5665ea7fe35f70468
CableBK <dev.witchapat@gmail.com>
2024-11-17 22:42:41 +0700

    Update Ontapchange WIP

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:iconly/iconly.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,8 +16,47 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF5F5F1),
-      bottomNavigationBar: GNav(
-        tabs: [],
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 12.0),
+          child: GNav(
+            backgroundColor: Color.fromARGB(255, 0, 0, 0),
+            color: Colors.white,
+            activeColor: Colors.white,
+            gap: 8,
+/*             onTabChange: (index) {
+              if (index == 0) {
+
+              } else if (condition2) {
+                // statement 2
+              } else if (condition3) {
+                // statement 3
+              } else {
+                // else statment
+              }
+            }, */
+            padding: EdgeInsets.all(18),
+            tabs: const [
+              GButton(
+                icon: IconlyLight.home,
+                text: 'หน้าแรก',
+              ),
+              GButton(
+                icon: IconlyLight.send,
+                text: 'NULL',
+              ),
+              GButton(
+                icon: IconlyLight.search,
+                text: 'ค้นหา',
+              ),
+              GButton(
+                icon: IconlyLight.setting,
+                text: 'ตั้งค่า',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
